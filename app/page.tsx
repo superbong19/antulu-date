78357528ef53af4f3b769207aafb7be645277ef7
superbong19 <congluan19092003@gmail.com>
2024-07-07 20:18:39 +0700

    init project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import FormLogin from "@/components/FormLogin";
+import Header from "@/components/Header";
+export default function Home() {
+
+  return (
+    <>
+      <Header />
+      <FormLogin />
+    </>
+  );
+}
